96565dc869ef2ab926939b5789a08bde7bf53e2c
AppRoyale <andre.schiemann@outlook.com>
2015-10-25 08:07:15 +0000

    boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,50 @@
 var path = require('path');
-var webpack = require('webpack');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+var merge = require('webpack-merge');
 var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+var Promise = require('es6-promise').Promise;
 
+var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, 'app');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 
-module.exports = {
+var common = {
   entry: APP_PATH,
   output: {
     path: BUILD_PATH,
     filename: 'bundle.js'
   },
-  devServer: {
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-    progress: true
+  module: {
+    loaders: [{
+      test: /\.css$/,
+      loaders: ['style', 'css'],
+      include: APP_PATH
+    }]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new HtmlwebpackPlugin({
-      title: 'Kanban app'
+      title: 'Kanban app',
     }),
     new OpenBrowserPlugin({
-      url: 'http://localhost:8080'
-    })
+      url: 'http://localhost:1337'
+    }),
   ]
 };
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      port: '1337',
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
